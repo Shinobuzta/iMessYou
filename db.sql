bcc4a5aea5b85fd1c753ca9ecdc2fe1126287875
Alexander Delos Santos <alex@codesignate.com>
2014-07-26 22:24:23 +0800

    added notes

---
+++ db.sql
@@ -0,0 +1,28 @@
+create table users(
+	id int(11) not null auto_increment,
+	username varchar(45) not null,
+	imei varchar(200) not null,
+	contacts text not null,
+	primary key (id)
+);
+
+create table images(
+	id int(11) not null auto_increment,
+	sndr_id int(11) not null,
+	rcvr_id int(11) not null,
+	image_path varchar(255) not null,
+	viewed enum('0','1') default '0' not null,
+	date_created timestamp not null,
+	primary key (id),
+	foreign key (sndr_id) references users(id),
+	foreign key (rcvr_id) references users(id)
+);
+
+create table images(
+	id int(11) not null auto_increment,
+	user_id int(11) not null,
+	contact_id int(11) not null,
+	primary key (id),
+	foreign key (user_id) references users(id),
+	foreign key (contact_id) references users(id)
+);
